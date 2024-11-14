7d52e2e8c89470f0fa644e88b5a8030a20713e58
Nikita Bendre <nickybendre898@gmail.com>
2024-11-14 23:39:05 +0530

    First Project Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = '10November';
+
+  No1: number = 0;
+  No2: number = 0;
+  Result: number = 0;
+
+
+  Addition() {
+    this.Result = this.No1 + this.No2;
+    console.log("Addition of two numbers")
+    return this.Result
+  }
+
+  Substraction() {
+    this.Result = this.No1 - this.No2;
+    console.log("Substraction of two number is: ")
+  }
+
+  Multiplication() {
+    this.Result = this.No1 * this.No2;
+    console.log("Multiplication of two number")
+  }
+
+  Division() {
+    this.Result = this.No1 / this.No2;
+    console.log("Division of two number")
+  }
+
 }
+
